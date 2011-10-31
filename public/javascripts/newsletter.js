aece999e7386142a010af59e07e3c0f0bb357353
Anibal Cucco <anibalcucco@gmail.com>
2011-10-31 18:54:16 -0300

    Add newsletter form placeholders through JS because refinery removes them after saving the edit page form

---
+++ public/javascripts/newsletter.js
@@ -0,0 +1,7 @@
+(function($) {
+  $(document).ready(function() {
+    var form = $("#newsletter_form");
+    form.find("#Field5").attr("placeholder", "Your Name");
+    form.find("#Field3").attr("placeholder", "Your Email");
+  });
+}(this.jQuery));
