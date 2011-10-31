aa545e7e43b5023963ab0edc94c81239ac90bf51
Anibal Cucco <anibalcucco@gmail.com>
2011-10-31 17:55:41 -0300

    Adding missing js file

---
+++ public/javascripts/facebook.js
@@ -0,0 +1,38 @@
+(function($) {
+  var element = null;
+  var appId   = null;
+
+  function fitInFacebookApp() {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: appId,
+        status: true,
+        cookie: true,
+        xfbml: true
+      });
+      //this resizes the the i-frame
+      //on an interval of 100ms
+      FB.Canvas.setAutoResize(100);
+    };
+    (function() {
+      var e = document.createElement('script');
+      e.async = true;
+      e.src = document.location.protocol +
+       '//connect.facebook.net/en_US/all.js';
+      element.append(e);
+    }());
+  }
+
+  function hidOverflow() {
+    $("html,body").css("overflow", "hidden");
+  }
+
+  $(document).ready(function() {
+    element = $("#fb-root");
+    appId   = element.data("app");
+    if (appId) {
+      hidOverflow();
+      fitInFacebookApp();
+    }
+  });
+}(this.jQuery));
