a91a452765e82a88f8f975163a4d57c30a55b8f5
Anibal Cucco <anibalcucco@gmail.com>
2011-11-01 15:34:43 -0300

    WIP: Add basic support for youtube videos

--- public/javascripts/videos.js
+++ public/javascripts/videos.js
@@ -5,11 +5,39 @@
   var current  = null;
   var item     = null;
 
-  function load() {
+  function vimeo() {
     var url = "http://vimeo.com/api/v2/album/" + album + "/videos.json?callback=?";
+    var videoURL = "http://player.vimeo.com/video/";
     $.getJSON(url, {}, function(data) {
       videos = data;
       $.each(videos, function(key, video) {
+        video.url = videoURL + video.id;
+        carousel.append(li(video));
+      });
+      if (videos.length > 0) {
+        carousel.jcarousel({ scroll: 1 });
+        show(videos[0].id);
+      } else {
+        carousel.html("No videos in album");
+      }
+    });
+  }
+
+  function youtube() {
+    var playListURL = "http://gdata.youtube.com/feeds/api/playlists/" + album + "?v=2&alt=json&callback=?";
+    var videoURL= "http://www.youtube.com/embed/";
+    $.getJSON(playListURL, function(data) {
+      feed = data;
+      $.each(data.feed.entry, function(i, item) {
+        var title = item.title.$t;
+        var description = item.media$group.media$description.$t;
+        var feedURL = item.link[1].href;
+        var fragments = feedURL.split("/");
+        var id = fragments[fragments.length - 2];
+        var url = videoURL + id;
+        var thumb = "http://img.youtube.com/vi/" + id + "/default.jpg";
+        var video = {id: id, title: title, description: description, url: url, thumbnail_small: thumb};
+        videos.push(video);
         carousel.append(li(video));
       });
       if (videos.length > 0) {
@@ -32,7 +60,7 @@
   function get(id) {
     for (var i = 0, len = videos.length; i<len; i++) {
       var video = videos[i];
-      if (video.id == parseInt(id)) {
+      if (video.id == id) {
         return video;
       }
     };
@@ -53,7 +81,7 @@
     item     = $("#videos .jcarousel-item");
 
     if (carousel.length) {
-      load();
+      vimeo();
       setupCarousel();
     }
   });
