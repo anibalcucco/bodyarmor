8800ea20824bd7519d8c4619e6ef2036efe6729f
Anibal Cucco <anibalcucco@gmail.com>
2011-11-04 12:15:04 -0300

    Changes to the Events section.
    * When you select an event in the pulldown we show the event description and a "view more" link that takes to the event's page.
    * The pictures in the carousel are always the same, they are not related to the selected event. On click redirect to the album's page.

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -1,17 +1,12 @@
 (function($) {
 
   $(document).ready(function() {
+    $("#events_carousel").jcarousel({ scroll: 1 });
     $("#events select").change(function() {
       var id = $(this).find("option:selected").first().val();
-
-      var event = { id: id,
-                    description: $("#" + id).find(".description").html(),
-                    url: $("#" + id).find(".url").html(),
-                    items: $("#" + id).find(".item") };
-
-      var element = $("#events #current_event").html($("#event_template").tmpl(event));
-
-      $(element).find('ul').jcarousel({ scroll: 1 });
+      var event = $("#" + id);
+      event.show();
+      $(".event").not(event).hide();
     }).trigger('change');
   });
 
